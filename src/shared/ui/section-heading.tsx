f939d74e58fd85ebb96d861ea492509059c8b24e
ijagamino <ijagamino@gmail.com>
2026-04-04 03:16:47 +0800

    feat: add background to texts

--- src/shared/ui/section-heading.tsx
+++ src/shared/ui/section-heading.tsx
@@ -1,15 +1,24 @@
-import type { ComponentProps, ReactNode } from "react"
-import { cn } from "@/shared/lib/cn"
+import type { ComponentProps, ReactNode } from "react";
+import { cn } from "@/shared/lib/cn";
 
 interface SectionHeadingProps extends ComponentProps<"h2"> {
-  children: ReactNode
+  children: ReactNode;
 }
 
-export default function SectionHeading({ children, className, ...props }: SectionHeadingProps) {
-
+export default function SectionHeading({
+  children,
+  className,
+  ...props
+}: SectionHeadingProps) {
   return (
-    <h2 className={cn("text-5xl md:text-7xl font-extrabold text-center", className)} {...props}>
-      {children}
+    <h2
+      className={cn(
+        "text-5xl md:text-7xl font-extrabold text-center px-2 py-1 rounded",
+        className,
+      )}
+      {...props}
+    >
+      <span className="bg-primary">{children}</span>
     </h2>
-  )
+  );
 }
--- src/shared/ui/section-introduction.tsx
+++ src/shared/ui/section-introduction.tsx
@@ -1,37 +1,41 @@
-import type { ComponentProps, ReactNode } from "react"
-import { cn } from "@/shared/lib/cn"
-import { cva, type VariantProps } from "class-variance-authority"
+import type { ComponentProps, ReactNode } from "react";
+import { cn } from "@/shared/lib/cn";
+import { cva, type VariantProps } from "class-variance-authority";
 
 interface SectionIntroductionProps extends ComponentProps<"p"> {
-  children: ReactNode
+  children: ReactNode;
 }
 
 const sectionIntroductionVariants = cva(
-  "absolute text-4xl font-medium text-center text-primary-foreground", {
-  variants: {
-    centered: {
-      false: null,
-      true: "top-1/2 left-1/2 -translate-1/2"
-    }
+  "absolute text-4xl font-medium px-2 py-1 rounded text-center text-primary-foreground",
+  {
+    variants: {
+      centered: {
+        false: null,
+        true: "top-1/2 left-1/2 -translate-1/2",
+      },
+    },
+    defaultVariants: {
+      centered: false,
+    },
   },
-  defaultVariants: {
-    centered: false
-  }
-})
+);
 
-export default function SectionIntroduction({ children, centered, className, ...props }: SectionIntroductionProps & VariantProps<typeof sectionIntroductionVariants>) {
+export default function SectionIntroduction({
+  children,
+  centered,
+  className,
+  ...props
+}: SectionIntroductionProps &
+  VariantProps<typeof sectionIntroductionVariants>) {
   return (
     <>
-      <p className={cn(sectionIntroductionVariants({ centered, className }))} {...props}>
-        <span className="
-          bg-primary
-          px-2 py-1
-          rounded
-        ">
-          {children}
-        </span>
+      <p
+        className={cn(sectionIntroductionVariants({ centered, className }))}
+        {...props}
+      >
+        <span className="bg-primary">{children}</span>
       </p>
     </>
-  )
+  );
 }
-
