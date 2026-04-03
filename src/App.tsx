07ce31e07d8c26cfa572e47156e2cf80f2fd834e
ijagamino <ijagamino@gmail.com>
2026-04-04 03:17:10 +0800

    feat: add smooth scrolling

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,4 @@
+import { ThemeProvider } from "@/app/providers/theme";
 import DustButton from "@/features/theme-switcher/ui/dust-button";
 import ContactSection from "@/widgets/contact-section/ui/section";
 import HeroSection from "@/widgets/hero-section/ui/section";
@@ -5,28 +6,44 @@ import IntroductionSection from "@/widgets/introduction-section/ui/section";
 import ProjectsSection from "@/widgets/projects-section/ui/section";
 import SeminarsSection from "@/widgets/seminars-section/ui/section";
 import ToolsSection from "@/widgets/tools-section/ui/section";
-import { ThemeProvider } from "@/app/providers/theme";
 import { useGSAP } from "@gsap/react";
 import gsap from "gsap";
-import { Flip, MotionPathPlugin, ScrollTrigger, SplitText, TextPlugin } from "gsap/all";
-import { useRef, useState } from "react";
+import { Flip, MotionPathPlugin, ScrollSmoother, ScrollTrigger, SplitText, TextPlugin } from "gsap/all";
+import { useState } from "react";
 
-gsap.registerPlugin(useGSAP)
-gsap.registerPlugin(Flip)
-gsap.registerPlugin(MotionPathPlugin)
-gsap.registerPlugin(ScrollTrigger)
-gsap.registerPlugin(SplitText)
-gsap.registerPlugin(TextPlugin)
+gsap.registerPlugin(Flip, MotionPathPlugin, ScrollSmoother, ScrollTrigger, SplitText, TextPlugin)
 
 function App() {
-  const mainRef = useRef<HTMLElement>(null);
   const [heroElement, setHeroElement] = useState<HTMLElement | null>(null)
+
+  useGSAP(() => {
+    ScrollSmoother.get()?.kill();
+
+    const smoother = ScrollSmoother.create({
+      wrapper: "#smooth-wrapper",
+      content: "#smooth-content",
+      smooth: 1.5,
+      effects: true,
+      smoothTouch: 0.1,
+    });
+
+    smoother.scrollTo(0, false)
+    ScrollTrigger.refresh();
+
+    return () => {
+      smoother.kill();
+    };
+  });
+
 
   return (
     <ThemeProvider>
-      <main
-        ref={mainRef}
-        className="
+      <DustButton triggerElement={heroElement} />
+      {/* <DustButton triggerElement={heroRef.current} /> */}
+      <div id="smooth-wrapper">
+        <div id="smooth-content">
+          <main
+            className="
         relative
         [&>section]:p-8
         [&>section]:min-h-dvh 
@@ -35,15 +52,16 @@ function App() {
         [&>section]:md:min-w-full
         [&>section>div]:mx-auto
         "
-      >
-        <DustButton triggerElement={heroElement} />
-        <HeroSection ref={setHeroElement} />
-        <IntroductionSection />
-        <ToolsSection />
-        <ProjectsSection />
-        <SeminarsSection />
-        <ContactSection />
-      </main>
+          >
+            <HeroSection ref={setHeroElement} />
+            <IntroductionSection />
+            <ToolsSection />
+            <ProjectsSection />
+            <SeminarsSection />
+            <ContactSection />
+          </main>
+        </div>
+      </div>
     </ThemeProvider>
   )
 }
